7c80c30c68250b552b88c0c2f21ae29faf209960
Jonathan <creaw15@gmail.com>
2022-04-15 16:01:57 -0400

    first commit.

---
+++ src/app.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var configuration = require('./app-config');
+
+var opts = {
+  maxPoolSize: 10,
+  autoIndex: false,
+  loggerLevel: "error", 
+  appname: "ripley-app",
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+var connectString = configuration.mongodb.connectionString;
+mongoose.connect(connectString, opts, function (err) {
+  if (err) throw err;
+  console.log("==> Conexión establecida con MongoDB");
+});
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(configuration.server.port, function () {
+  console.log(`Listening on port ${configuration.server.port}`);
+});
