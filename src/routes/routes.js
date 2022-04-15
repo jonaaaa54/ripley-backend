4e67debde31bede5081f42baf907557195b6ae6c
Jonathan <creaw15@gmail.com>
2022-04-15 16:08:57 -0400

    added new templates and update redirect in routes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,4 +3,12 @@ var router = express.Router();
 
 const pug = require('pug');
 
+router.get('/', function(req, res, next) {
+  res.send(pug.renderFile(__dirname + '../templates/api-index.pug'));
+});
+
+router.get('/*', function(req, res, next) {
+  res.status(400).send({message: 'Invalid API endpoint'});
+});
+
 module.exports = router;
